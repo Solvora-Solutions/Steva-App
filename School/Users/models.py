2411aa383e62c3e0d50335b20f14c62f9ba9d283
Emmanuel Ekow Tawiah Panford <ekowtawiah23@gmail.com>
2025-07-13 23:08:36 +0000

    Add User model

--- School/Users/models.py
+++ School/Users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('parent', 'Parent'),
+        ('staff', 'Staff'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='parent')
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'username'  # Change to 'email' if you want email login
+    REQUIRED_FIELDS = ['email']  # This is used when creating a superuser
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
