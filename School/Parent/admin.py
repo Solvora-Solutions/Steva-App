f15e8a3da1003cb2bc076349a4dc8f6c3b73a429
Emmanuel Ekow Tawiah Panford <ekowtawiah23@gmail.com>
2025-08-18 22:06:28 +0000

    Update to Parent model

--- School/Parent/admin.py
+++ School/Parent/admin.py
@@ -1,21 +1,16 @@
 from django.contrib import admin
 from .models import Parent
-from .forms import ParentForm
 
 
 @admin.register(Parent)
 class ParentAdmin(admin.ModelAdmin):
-    form = ParentForm
     list_display = ("full_name", "phone_number", "verified", "is_primary", "created_at")
     list_filter = ("verified", "is_primary", "created_at")
     search_fields = ("user__first_name", "user__last_name", "phone_number", "user__email")
     ordering = ("user__first_name", "user__last_name")
 
     fieldsets = (
-        ("User Information", {
-            "fields": ("first_name", "last_name", "email", "password1", "password2"),
-        }),
         ("Parent Profile", {
-            "fields": ("phone_number", "is_primary", "verified"),
+            "fields": ("user", "phone_number", "is_primary", "verified"),
         }),
     )
